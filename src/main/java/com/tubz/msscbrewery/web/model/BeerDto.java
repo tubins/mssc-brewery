4cdb82d56d00e81335b2e98c1f1bbbd257f381f8
tubins <tubin.shaji@gmail.com>
2022-05-04 02:43:44 +0100

    Fix variable name of beer id

--- src/main/java/com/tubz/msscbrewery/web/model/BeerDto.java
+++ src/main/java/com/tubz/msscbrewery/web/model/BeerDto.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
-    private UUID uuid;
+    private UUID id;
     private String beerName;
     private String beerStyle;
     private Long upc;
